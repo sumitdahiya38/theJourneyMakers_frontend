0ba17c2fede1c50c6f8b2d72478283ddbaee30f0
sumitdahiyail <sumit.dahiya@infinitelocus.com>
2025-07-01 17:55:50 +0530

    sheet integration

--- script.js
+++ script.js
@@ -62,3 +62,26 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   
+
+  window.sendFormToSheet =  async function() {
+    const id = 'AKfycbxvn0J86tJU6Qmb_8hNENyEwqJpgQOKQtV7v29S5FuFZH3wpW5imTtMKJ2P3d8UlfAX0A';
+    const form = document.getElementById('contactForm');
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+  
+    try {
+      const response = await fetch(`https://script.google.com/macros/s/${id}/exec`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      await response.json();
+      alert('Form Submitted successfully!');
+    } catch (error) {
+      alert('Error submitting Form.');
+      console.error(error);
+    }
+  }
